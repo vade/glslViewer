d2dbb6999f450c8fe30ce7bf542fd1f3d7c472e0
vade <vade@vade.info>
2021-12-29 15:55:39 -0500

    remove another clear?

--- src/main.cpp
+++ src/main.cpp
@@ -105,7 +105,7 @@ void loop() {
 #endif
     ada::updateGL();
 
-    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+    //glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
     #ifndef __EMSCRIPTEN__
     if (!bTerminate && !fullFps && !sandbox.haveChange()) {
